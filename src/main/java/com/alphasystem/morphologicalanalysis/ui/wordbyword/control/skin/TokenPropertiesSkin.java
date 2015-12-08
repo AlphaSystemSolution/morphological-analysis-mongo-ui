81b86d25361470553e44e4376f3321f0c5fc66f9
sali <sali2@loyalty.com>
2015-12-08 16:49:30 -0500

    adding browser tab

--- src/main/java/com/alphasystem/morphologicalanalysis/ui/wordbyword/control/skin/TokenPropertiesSkin.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/ui/wordbyword/control/skin/TokenPropertiesSkin.java
@@ -1,10 +1,12 @@
 package com.alphasystem.morphologicalanalysis.ui.wordbyword.control.skin;
 
+import com.alphasystem.arabic.ui.Browser;
 import com.alphasystem.morphologicalanalysis.ui.common.LocationListCell;
 import com.alphasystem.morphologicalanalysis.ui.wordbyword.control.LocationPropertiesView;
 import com.alphasystem.morphologicalanalysis.ui.wordbyword.control.TokenPropertiesView;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.Location;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.Token;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.support.RootWord;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
@@ -13,6 +15,7 @@ import javafx.scene.layout.VBox;
 import java.util.List;
 
 import static com.alphasystem.morphologicalanalysis.ui.common.Global.*;
+import static java.lang.String.format;
 import static javafx.geometry.Pos.CENTER;
 import static javafx.scene.control.TabPane.TabClosingPolicy.UNAVAILABLE;
 
@@ -24,6 +27,8 @@ public class TokenPropertiesSkin extends SkinBase<TokenPropertiesView> {
     private final ComboBox<Location> locationComboBox;
     private final LocationPropertiesView locationPropertiesView;
     private final TabPane tabPane;
+    private final Browser browser;
+    private Tab browseDictionaryTab;
 
     public TokenPropertiesSkin(TokenPropertiesView control) {
         super(control);
@@ -33,6 +38,10 @@ public class TokenPropertiesSkin extends SkinBase<TokenPropertiesView> {
         tabPane.setBorder(BORDER);
         locationComboBox = new ComboBox<>();
         locationPropertiesView = new LocationPropertiesView();
+        browser = new Browser();
+        browseDictionaryTab = new Tab("Browse Dictionary", browser);
+        browseDictionaryTab.setDisable(true);
+
         initializeSkin();
     }
 
@@ -56,13 +65,31 @@ public class TokenPropertiesSkin extends SkinBase<TokenPropertiesView> {
         locationComboBox.getSelectionModel().selectedItemProperty().addListener((o, ov, nv) -> {
             if (nv != null) {
                 locationPropertiesView.setLocation(nv);
+                tabPane.getSelectionModel().select(0);
+                loadDictionary(nv);
             }
         });
         gridPane.setBorder(BORDER);
         return gridPane;
     }
 
+    private void loadDictionary(Location nv) {
+        RootWord rootWord = nv.getRootWord();
+        String searchString = null;
+        boolean disableDictionaryTab = rootWord == null;
+        if (!disableDictionaryTab) {
+            searchString = rootWord.toCode();
+        }
+        disableDictionaryTab = searchString == null;
+        browseDictionaryTab.setDisable(disableDictionaryTab);
+        if (!disableDictionaryTab) {
+            String url = format("%s%s", MAWRID_READER_URL, searchString);
+            browser.loadUrl(url);
+        }
+    }
+
     private void updateLocations(Token token) {
+        tabPane.getSelectionModel().select(0);
         locationComboBox.getItems().clear();
         locationComboBox.setDisable(true);
         if (token == null) {
@@ -81,7 +108,8 @@ public class TokenPropertiesSkin extends SkinBase<TokenPropertiesView> {
         VBox vBox = new VBox();
         vBox.setSpacing(GAP);
 
-        tabPane.getTabs().add(new Tab(RESOURCE_BUNDLE.getString("locationProperties.label"), locationPropertiesView));
+        tabPane.getTabs().addAll(new Tab(RESOURCE_BUNDLE.getString("locationProperties.label"), locationPropertiesView),
+                browseDictionaryTab);
 
         vBox.getChildren().addAll(createLocationsPane(), tabPane);
         getChildren().add(vBox);
