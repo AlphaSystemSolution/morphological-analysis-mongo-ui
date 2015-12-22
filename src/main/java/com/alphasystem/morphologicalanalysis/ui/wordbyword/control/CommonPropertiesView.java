66060940ce720996abc5660ff64093479bc13fb9
sali <sali2@loyalty.com>
2015-12-22 08:36:59 -0500

    Simplified logic to change properties, now logic will be handled in model layer

--- src/main/java/com/alphasystem/morphologicalanalysis/ui/wordbyword/control/CommonPropertiesView.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/ui/wordbyword/control/CommonPropertiesView.java
@@ -1,7 +1,6 @@
 package com.alphasystem.morphologicalanalysis.ui.wordbyword.control;
 
 import com.alphasystem.morphologicalanalysis.ui.wordbyword.control.skin.CommonPropertiesSkin;
-import com.alphasystem.morphologicalanalysis.wordbyword.model.AbstractProperties;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.Location;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.support.NamedTag;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.support.PartOfSpeech;
@@ -24,32 +23,7 @@ public class CommonPropertiesView extends Control {
 
     public CommonPropertiesView() {
         locationProperty().addListener((o, ov, nv) -> setValues(nv));
-        partOfSpeechProperty().addListener((o, ov, nv) -> {
-            // setting part of speech will make properties re-initialized, which will override the location
-            // properties, there is a case where we don't want to have this override to be happened.
-
-            // Case 1: We already have value in the DB and from UI we selected the location from drop down,
-            // from "LocationPropertiesSkin" line 47 we changed the common properties view (this class) and
-            // that will trigger the change in part of speech, that's why we are here.
-            // Now we need to change the part of speech in UI but will reset the properties as well which is we
-            // want to be happened. So we want to keep track current sets of properties and put them back once
-            // part of speech is set
-
-            // Case 2: This is completely new sets of properties, when properties are initialized by default they
-            // are assigned "NOUN" as part of speech, now if we are changing to some other type of part of speech
-            // then holding of current sets of properties will not work, in this case we want the override to be
-            // happened
-
-            Location location = getLocation();
-            AbstractProperties properties = location.getProperties();
-            location.setPartOfSpeech((nv == null) ? NOUN : nv);
-
-            // trick is to find out that if current location is transient or not, sets the properties back if and
-            // only if the location is not transient (means it does not have start and end index set.
-            if (!location.isTransient()) {
-                location.setProperties(properties);
-            }
-        });
+        partOfSpeechProperty().addListener((o, ov, nv) -> getLocation().setPartOfSpeech(nv));
         namedTagProperty().addListener((o, ov, nv) -> getLocation().setNamedTag(nv));
         translationProperty().addListener((o, ov, nv) -> getLocation().setTranslation(nv));
     }
@@ -72,10 +46,6 @@ public class CommonPropertiesView extends Control {
         return location;
     }
 
-    public final PartOfSpeech getPartOfSpeech() {
-        return partOfSpeech.get();
-    }
-
     public final void setPartOfSpeech(PartOfSpeech partOfSpeech) {
         this.partOfSpeech.set((partOfSpeech == null) ? NOUN : partOfSpeech);
     }
@@ -84,20 +54,12 @@ public class CommonPropertiesView extends Control {
         return partOfSpeech;
     }
 
-    public final NamedTag getNamedTag() {
-        return namedTag.get();
-    }
-
     public final void setNamedTag(NamedTag namedTag) {
         this.namedTag.set(namedTag);
     }
 
     public final ObjectProperty<NamedTag> namedTagProperty() {
         return namedTag;
-    }
-
-    public final String getTranslation() {
-        return translation.get();
     }
 
     public final void setTranslation(String translation) {
