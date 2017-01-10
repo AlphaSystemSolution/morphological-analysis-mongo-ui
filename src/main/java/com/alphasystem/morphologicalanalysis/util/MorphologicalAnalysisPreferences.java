4cce16ae12087f5a14846521d3347e31160c67ce
sfali23 <f.syed.ali@gmail.com>
2017-01-10 07:58:33 -0500

    preferences for fonts

---
+++ src/main/java/com/alphasystem/morphologicalanalysis/util/MorphologicalAnalysisPreferences.java
@@ -0,0 +1,13 @@
+package com.alphasystem.morphologicalanalysis.util;
+
+import com.alphasystem.app.morphologicalengine.ui.util.MorphologicalEnginePreferences;
+
+/**
+ * @author sali
+ */
+public class MorphologicalAnalysisPreferences extends MorphologicalEnginePreferences {
+
+    public MorphologicalAnalysisPreferences() {
+        super(MorphologicalAnalysisPreferences.class);
+    }
+}
