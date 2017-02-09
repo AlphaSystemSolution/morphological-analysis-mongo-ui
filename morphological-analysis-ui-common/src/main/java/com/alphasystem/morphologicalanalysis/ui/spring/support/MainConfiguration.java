f470b5715fa5fd77f910a5c6522b5ff43c7a5a62
sfali23 <f.syed.ali@gmail.com>
2017-02-09 12:54:02 -0500

    added dependency on spring-web to make rest calls

--- morphological-analysis-ui-common/src/main/java/com/alphasystem/morphologicalanalysis/ui/spring/support/MainConfiguration.java
+++ morphological-analysis-ui-common/src/main/java/com/alphasystem/morphologicalanalysis/ui/spring/support/MainConfiguration.java
@@ -1,7 +1,17 @@
-package com.alphasystem.morphologicalanalysis.ui.application;
+package com.alphasystem.morphologicalanalysis.ui.spring.support;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @author sali
  */
+@Configuration
 public class MainConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
