43a066cd837b63db74dd09a4170db3925e7c4b9d
sfali23 <f.syed.ali@gmail.com>
2017-02-09 20:10:55 -0500

    use object mapperto convert object

--- morphological-analysis-ui-common/src/main/java/com/alphasystem/morphologicalanalysis/ui/util/RepositoryService.java
+++ morphological-analysis-ui-common/src/main/java/com/alphasystem/morphologicalanalysis/ui/util/RepositoryService.java
@@ -1,12 +1,15 @@
-package com.alphasystem.morphologicalanalysis.ui.wordbyword.util;
+package com.alphasystem.morphologicalanalysis.ui.util;
 
 import com.alphasystem.morphologicalanalysis.wordbyword.model.Chapter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -21,8 +24,13 @@ public class RepositoryService {
 
     @PostConstruct
     @SuppressWarnings("unchecked")
-    public void postConstruct(){
-        chapters = restTemplate.getForObject(String.format("%s/chapters/", urlPath), List.class);
+    public void postConstruct() {
+        final List<LinkedHashMap> list = restTemplate.getForObject(String.format("%s/chapters/", urlPath), List.class);
+        chapters = new ArrayList<>();
+        ObjectMapper objectMapper = new ObjectMapper();
+        list.forEach(linkedHashMap -> {
+            chapters.add(objectMapper.convertValue(linkedHashMap, Chapter.class));
+        });
     }
 
     public List<Chapter> getChapters() {
