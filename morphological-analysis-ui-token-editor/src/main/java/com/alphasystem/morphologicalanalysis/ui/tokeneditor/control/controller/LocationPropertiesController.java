fc7ca0a30965625002c40ed855977cf7111478a6
sfali23 <f.syed.ali@gmail.com>
2017-02-20 08:24:44 -0500

    VerbProperties controller

--- morphological-analysis-ui-token-editor/src/main/java/com/alphasystem/morphologicalanalysis/ui/tokeneditor/control/controller/LocationPropertiesController.java
+++ morphological-analysis-ui-token-editor/src/main/java/com/alphasystem/morphologicalanalysis/ui/tokeneditor/control/controller/LocationPropertiesController.java
@@ -1,7 +1,14 @@
 package com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.controller;
 
+import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.AbstractPropertiesView;
 import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.CommonPropertiesView;
 import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.LocationPropertiesView;
+import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.NounPropertiesView;
+import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.ParticlePropertiesView;
+import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.ProNounPropertiesView;
+import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.VerbPropertiesView;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.Location;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.support.WordType;
 import javafx.scene.layout.AnchorPane;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,16 +21,56 @@ import javax.annotation.PostConstruct;
 @Component
 public class LocationPropertiesController extends AnchorPane {
 
+    private static final double DEFAULT_OFFSET = 10.0;
+
     @Autowired private LocationPropertiesView control;
     @Autowired private CommonPropertiesView commonPropertiesView;
+    @Autowired private NounPropertiesView nounPropertiesView;
+    @Autowired private ProNounPropertiesView proNounPropertiesView;
+    @Autowired private VerbPropertiesView verbPropertiesView;
+    @Autowired private ParticlePropertiesView particlePropertiesView;
 
     @PostConstruct
-    void postConstruct(){
-       commonPropertiesView.setLocation(control.getLocation());
-        control.locationProperty().addListener((observable, oldValue, newValue) -> {
-            commonPropertiesView.setLocation(newValue);
-        });
-        setLeftAnchor(commonPropertiesView, 10.0);
-        getChildren().addAll(commonPropertiesView);
+    void postConstruct() {
+        refresh(control.getLocation());
+        control.locationProperty().addListener((observable, oldValue, newValue) -> refresh(newValue));
+    }
+
+    private void refresh(Location location) {
+        getChildren().clear();
+        commonPropertiesView.setLocation(location);
+        final AbstractPropertiesView propertiesView = getPropertiesView(location);
+        setLeftAnchor(commonPropertiesView, DEFAULT_OFFSET);
+        setTopAnchor(commonPropertiesView, DEFAULT_OFFSET);
+        setLeftAnchor(propertiesView, 470 + DEFAULT_OFFSET);
+        setTopAnchor(propertiesView, DEFAULT_OFFSET);
+        getChildren().addAll(commonPropertiesView, propertiesView);
+    }
+
+    @SuppressWarnings({"unchecked"})
+    private AbstractPropertiesView getPropertiesView(Location location) {
+        if (location == null) {
+            return nounPropertiesView;
+        }
+        AbstractPropertiesView node = null;
+        final WordType wordType = location.getWordType();
+        switch (wordType) {
+            case NOUN:
+                node = nounPropertiesView;
+                break;
+            case PRO_NOUN:
+                node = proNounPropertiesView;
+                break;
+            case VERB:
+                node = verbPropertiesView;
+                break;
+            case PARTICLE:
+                node = particlePropertiesView;
+                break;
+        }
+        if (!WordType.PARTICLE.equals(wordType)) {
+            node.setLocationProperties(location.getProperties().get(0));
+        }
+        return node;
     }
 }
---
+++ morphological-analysis-ui-token-editor/src/main/java/com/alphasystem/morphologicalanalysis/ui/tokeneditor/control/controller/VerbPropertiesController.java
@@ -0,0 +1,94 @@
+package com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.controller;
+
+import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.VerbPropertiesView;
+import com.alphasystem.morphologicalanalysis.ui.util.ApplicationHelper;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.support.ConversationType;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.support.GenderType;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.support.IncompleteVerbCategory;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.support.IncompleteVerbType;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.support.NumberType;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.support.VerbMode;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.support.VerbPartOfSpeechType;
+import com.alphasystem.morphologicalanalysis.wordbyword.model.support.VerbType;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.BorderPane;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static com.alphasystem.morphologicalanalysis.wordbyword.model.support.IncompleteVerbCategory.DUMMY;
+
+/**
+ * @author sali
+ */
+@Component
+public class VerbPropertiesController extends BorderPane {
+
+    @Autowired private VerbPropertiesView control;
+    @FXML private ComboBox<VerbPartOfSpeechType> partOfSpeechComboBox;
+    @FXML private ComboBox<VerbType> verbTypeComboBox;
+    @FXML private ComboBox<ConversationType> conversationTypeComboBox;
+    @FXML private ComboBox<NumberType> numberTypeComboBox;
+    @FXML private ComboBox<GenderType> genderTypeComboBox;
+    @FXML private ComboBox<VerbMode> verbModeComboBox;
+    @FXML private ComboBox<IncompleteVerbCategory> incompleteVerbCategoryComboBox;
+    @FXML private ComboBox<IncompleteVerbType> incompleteVerbTypeComboBox;
+
+
+    @PostConstruct
+    void postConstruct() throws IOException, URISyntaxException {
+        ApplicationHelper.loadFxml(control, this);
+    }
+
+    @FXML
+    void initialize() {
+        partOfSpeechComboBox.valueProperty().bindBidirectional(control.partOfSpeechTypeProperty());
+        verbTypeComboBox.valueProperty().bindBidirectional(control.verbTypeProperty());
+        conversationTypeComboBox.valueProperty().bindBidirectional(control.conversationTypeProperty());
+        numberTypeComboBox.valueProperty().bindBidirectional(control.numberTypeProperty());
+        genderTypeComboBox.valueProperty().bindBidirectional(control.genderTypeProperty());
+        verbModeComboBox.valueProperty().bindBidirectional(control.verbModeProperty());
+
+        incompleteVerbCategoryComboBox.getSelectionModel().selectFirst();
+        setIncompleteVerbCategoryComboBoxValue(false, control.getIncompleteVerbCategory(), control.getIncompleteVerbType());
+        control.incompleteVerbCategoryProperty().addListener((observable, oldValue, newValue) ->
+                setIncompleteVerbCategoryComboBoxValue(false, newValue, null));
+        control.incompleteVerbTypeProperty().addListener((observable, oldValue, newValue) -> {
+            if (!incompleteVerbTypeComboBox.isDisabled()) {
+                incompleteVerbTypeComboBox.setValue(newValue);
+            }
+        });
+
+        incompleteVerbTypeComboBox.setDisable(true);
+        incompleteVerbCategoryComboBox.valueProperty().addListener((o, ov, nv) -> {
+            control.setIncompleteVerbCategory(nv);
+            setIncompleteVerbCategoryComboBoxValue(true, nv, null);
+        });
+        incompleteVerbTypeComboBox.valueProperty().addListener((observable, oldValue, newValue) -> control.setIncompleteVerbType(newValue));
+    }
+
+    private void setIncompleteVerbCategoryComboBoxValue(boolean self, final IncompleteVerbCategory incompleteVerbCategory,
+                                                        final IncompleteVerbType incompleteVerbType) {
+        IncompleteVerbCategory newValue = ((incompleteVerbCategory == null) || DUMMY.equals(incompleteVerbCategory)) ?
+                null : incompleteVerbCategory;
+        if (!self) {
+            incompleteVerbCategoryComboBox.setValue(newValue);
+        }
+        boolean disable = newValue == null;
+        incompleteVerbTypeComboBox.getItems().clear();
+        incompleteVerbTypeComboBox.setDisable(disable);
+        if (!disable) {
+            incompleteVerbTypeComboBox.getItems().addAll(incompleteVerbCategory.getMembers());
+            if (incompleteVerbType == null) {
+                incompleteVerbTypeComboBox.getSelectionModel().selectFirst();
+            } else {
+                incompleteVerbTypeComboBox.setValue(incompleteVerbType);
+            }
+        }
+    }
+}
+
