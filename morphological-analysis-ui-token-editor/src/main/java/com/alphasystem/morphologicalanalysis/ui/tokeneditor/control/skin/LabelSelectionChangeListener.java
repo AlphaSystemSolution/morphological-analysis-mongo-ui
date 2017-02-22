b79015bb1eb2bf9918825b7e2b6ead08c50f364d
sfali23 <f.syed.ali@gmail.com>
2017-02-22 17:48:36 -0500

    validation for selecting first letter

--- morphological-analysis-ui-token-editor/src/main/java/com/alphasystem/morphologicalanalysis/ui/tokeneditor/control/skin/LabelSelectionChangeListener.java
+++ morphological-analysis-ui-token-editor/src/main/java/com/alphasystem/morphologicalanalysis/ui/tokeneditor/control/skin/LabelSelectionChangeListener.java
@@ -3,12 +3,18 @@ package com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.skin;
 import com.alphasystem.arabic.model.ArabicWord;
 import com.alphasystem.arabic.ui.ArabicLabelView;
 import com.alphasystem.morphologicalanalysis.ui.tokeneditor.application.ApplicationController;
+import com.alphasystem.morphologicalanalysis.ui.util.ApplicationHelper;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.Location;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.Token;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.support.WordType;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 
 /**
  * @author sali
@@ -33,13 +39,26 @@ public class LabelSelectionChangeListener implements ChangeListener<Boolean> {
                     break;
                 }
             } // end of "for"
-            // TODO: validate
+            final Location location = model.getLocation();
+            if (location.getStartIndex() == index) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                Label label = new Label("This operation will leave current location without any element," +
+                        System.lineSeparator() + "a location must have at least one element in it.");
+                label.setAlignment(Pos.CENTER);
+                label.setTextAlignment(TextAlignment.CENTER);
+                label.setFont(Font.font(ApplicationHelper.PREFERENCES.getEnglishFontName(), 14));
+                alert.getDialogPane().setContent(label);
+                alert.setTitle("Invalid Operation");
+                alert.showAndWait();
+                model.getLabelView().setSelect(true);
+                return;
+            }
+            // TODO: more validation
             // update current location, create new, add location to token, and finally re-initialize
             final Token token = model.getControl().getToken();
             final ArabicWord tokenWord = token.tokenWord();
-            applicationController.updateLocation(model.getLocation(), index, tokenWord);
-            final Location newLocation = applicationController.createNewLocation(model.getLocation(), index,
-                    tokenWord, WordType.NOUN);
+            applicationController.updateLocation(location, index, tokenWord);
+            final Location newLocation = applicationController.createNewLocation(location, index, tokenWord, WordType.NOUN);
             token.addLocation(newLocation);
             model.getControl().setToken(null);
             model.getControl().setToken(token);
