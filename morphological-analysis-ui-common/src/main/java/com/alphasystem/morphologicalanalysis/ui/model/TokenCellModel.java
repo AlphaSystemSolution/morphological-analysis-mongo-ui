1e28524659ba5739ed6a20b68304ac931bb5a2ab
sfali23 <f.syed.ali@gmail.com>
2017-02-22 14:18:00 -0500

    changing model to use common TableCell

--- morphological-analysis-ui-common/src/main/java/com/alphasystem/morphologicalanalysis/ui/model/TokenCellModel.java
+++ morphological-analysis-ui-common/src/main/java/com/alphasystem/morphologicalanalysis/ui/model/TokenCellModel.java
@@ -1,16 +1,17 @@
 package com.alphasystem.morphologicalanalysis.ui.model;
 
 
+import com.alphasystem.arabic.model.ArabicLetterType;
+import com.alphasystem.arabic.model.ArabicLetters;
 import com.alphasystem.arabic.model.ArabicTool;
+import com.alphasystem.arabic.model.ArabicWord;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.Token;
 import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.ReadOnlyStringProperty;
-import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-
-import static java.lang.String.format;
 
 /**
  * @author sali
@@ -18,27 +19,37 @@ import static java.lang.String.format;
 public final class TokenCellModel {
 
     private final BooleanProperty checked;
-    private final ReadOnlyStringWrapper displayName;
-    private final ReadOnlyStringWrapper text;
+    private final ReadOnlyObjectWrapper<ArabicWord> displayName = new ReadOnlyObjectWrapper<>(this, "displayName");
+    private final ReadOnlyObjectWrapper<ArabicWord> text = new ReadOnlyObjectWrapper<>(this, "text");
     private final StringProperty morphologicalDescription;
     private Token token;
 
     public TokenCellModel(Token token) {
         this.token = token;
         checked = new SimpleBooleanProperty(false);
-        displayName = new ReadOnlyStringWrapper(getTokenNumber(this.token));
-        text = new ReadOnlyStringWrapper((token == null) ? "" : token.tokenWord().toUnicode());
+        displayName.set(getTokenNumber(this.token));
+        text.set(getText(this.token));
         morphologicalDescription = new SimpleStringProperty();
     }
 
-    private static String getTokenNumber(Token token) {
+    private static ArabicWord getTokenNumber(Token token) {
         if (token == null) {
-            return "";
+            return ArabicLetters.WORD_SPACE;
         }
-        String chapterNumber = ArabicTool.getArabicNumberWord(token.getChapterNumber()).toUnicode();
-        String verseNumber = ArabicTool.getArabicNumberWord(token.getVerseNumber()).toUnicode();
-        String tokenNumber = ArabicTool.getArabicNumberWord(token.getTokenNumber()).toUnicode();
-        return format("(%s:%s:%s)", chapterNumber, verseNumber, tokenNumber);
+        ArabicWord chapterNumber = ArabicTool.getArabicNumberWord(token.getChapterNumber());
+        ArabicWord verseNumber = ArabicTool.getArabicNumberWord(token.getVerseNumber());
+        ArabicWord tokenNumber = ArabicTool.getArabicNumberWord(token.getTokenNumber());
+        ArabicWord.concatenate(chapterNumber, ArabicWord.getWord(ArabicLetterType.SEMI_COLON), verseNumber,
+                ArabicWord.getWord(ArabicLetterType.SEMI_COLON), tokenNumber);
+        return ArabicWord.concatenate(chapterNumber, ArabicWord.getWord(ArabicLetterType.SEMI_COLON), verseNumber,
+                ArabicWord.getWord(ArabicLetterType.SEMI_COLON), tokenNumber);
+    }
+
+    private static ArabicWord getText(Token token){
+        if (token == null) {
+            return ArabicLetters.WORD_SPACE;
+        }
+        return token.tokenWord();
     }
 
     public Token getToken() {
@@ -57,19 +68,19 @@ public final class TokenCellModel {
         return checked;
     }
 
-    public final String getDisplayName() {
+    public final ArabicWord getDisplayName() {
         return displayName.get();
     }
 
-    public final ReadOnlyStringProperty displayNameProperty() {
+    public final ReadOnlyObjectProperty<ArabicWord> displayNameProperty() {
         return displayName.getReadOnlyProperty();
     }
 
-    public final String getText() {
+    public final ArabicWord getText() {
         return textProperty().get();
     }
 
-    public final ReadOnlyStringProperty textProperty() {
+    public final ReadOnlyObjectProperty<ArabicWord> textProperty() {
         return text.getReadOnlyProperty();
     }
 
--- morphological-analysis-ui-word-by-word/src/main/java/com/alphasystem/morphologicalanalysis/ui/wordbyword/control/TokenView.java
+++ morphological-analysis-ui-word-by-word/src/main/java/com/alphasystem/morphologicalanalysis/ui/wordbyword/control/TokenView.java
@@ -1,27 +1,24 @@
 package com.alphasystem.morphologicalanalysis.ui.wordbyword.control;
 
+import com.alphasystem.arabic.model.ArabicWord;
 import com.alphasystem.fx.ui.util.UiUtilities;
 import com.alphasystem.morphologicalanalysis.common.model.VerseTokenPairGroup;
 import com.alphasystem.morphologicalanalysis.common.model.VerseTokensPair;
-import com.alphasystem.morphologicalanalysis.ui.util.ApplicationHelper;
-import com.alphasystem.morphologicalanalysis.ui.util.MorphologicalAnalysisPreferences;
-import com.alphasystem.morphologicalanalysis.ui.util.RestClient;
+import com.alphasystem.morphologicalanalysis.ui.control.TextTableCell;
 import com.alphasystem.morphologicalanalysis.ui.model.TokenCellModel;
+import com.alphasystem.morphologicalanalysis.ui.util.ApplicationHelper;
+import com.alphasystem.morphologicalanalysis.ui.util.RestClient;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.Token;
-import com.alphasystem.util.GenericPreferences;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.NodeOrientation;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Screen;
 import javafx.util.Callback;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +26,6 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
-
-import static javafx.geometry.NodeOrientation.RIGHT_TO_LEFT;
-import static javafx.scene.control.ContentDisplay.GRAPHIC_ONLY;
 
 /**
  * @author sali
@@ -49,7 +43,6 @@ public class TokenView extends BorderPane {
         initialGroup.getPairs().add(new VerseTokensPair(4, 1));
     }
 
-    private final MorphologicalAnalysisPreferences preferences = GenericPreferences.getInstance(MorphologicalAnalysisPreferences.class);
     private final ObjectProperty<VerseTokenPairGroup> verseTokenPairGroup = new SimpleObjectProperty<>(this, "verseTokenPairGroup", initialGroup);
     private final BooleanProperty refresh = new SimpleBooleanProperty(this, "refresh", true);
     private TableView<TokenCellModel> tableView = new TableView<>();
@@ -71,17 +64,25 @@ public class TokenView extends BorderPane {
         descriptionColumn.setText("Description");
         descriptionColumn.setCellValueFactory(param -> param.getValue().morphologicalDescriptionProperty());
 
-        final TableColumn<TokenCellModel, String> tokenColumn = new TableColumn<>();
+        final TableColumn<TokenCellModel, ArabicWord> tokenColumn = new TableColumn<>();
         tokenColumn.setMinWidth(300);
         tokenColumn.setText("Text");
         tokenColumn.setCellValueFactory(param -> param.getValue().textProperty());
-        tokenColumn.setCellFactory(TextTableCell::new);
+        tokenColumn.setCellFactory(param -> {
+            TextTableCell<TokenCellModel, ArabicWord> cell = new TextTableCell<>(param);
+            cell.setFont(ApplicationHelper.PREFERENCES.getArabicFont30());
+            return cell;
+        });
 
-        final TableColumn<TokenCellModel, String> tokenNumberColumn = new TableColumn<>();
+        final TableColumn<TokenCellModel, ArabicWord> tokenNumberColumn = new TableColumn<>();
         tokenNumberColumn.setMinWidth(200);
         tokenNumberColumn.setText("Token Number");
         tokenNumberColumn.setCellValueFactory(param -> param.getValue().displayNameProperty());
-        tokenNumberColumn.setCellFactory(TextTableCell::new);
+        tokenNumberColumn.setCellFactory(param -> {
+            TextTableCell<TokenCellModel, ArabicWord> cell = new TextTableCell<>(param);
+            cell.setFont(ApplicationHelper.PREFERENCES.getArabicFont30());
+            return cell;
+        });
 
         final TableColumn<TokenCellModel, Boolean> checkBoxColumn = new TableColumn<>();
         checkBoxColumn.setMaxWidth(50);
@@ -132,27 +133,4 @@ public class TokenView extends BorderPane {
         this.refresh.set(refresh);
     }
 
-    private class TextTableCell extends TableCell<TokenCellModel, String> {
-        private final Text text;
-
-        private TextTableCell(TableColumn<TokenCellModel, String> column) {
-            setContentDisplay(GRAPHIC_ONLY);
-            text = new Text();
-            text.setFont(preferences.getArabicFont30());
-            text.setTextAlignment(TextAlignment.RIGHT);
-            text.setNodeOrientation(RIGHT_TO_LEFT);
-        }
-
-        @Override
-        protected void updateItem(String item, boolean empty) {
-            super.updateItem(item, empty);
-
-            BorderPane borderPane = new BorderPane();
-            if (item != null && !empty) {
-                text.setText(item);
-                borderPane.setCenter(text);
-            }
-            setGraphic(borderPane);
-        }
-    }
 }
