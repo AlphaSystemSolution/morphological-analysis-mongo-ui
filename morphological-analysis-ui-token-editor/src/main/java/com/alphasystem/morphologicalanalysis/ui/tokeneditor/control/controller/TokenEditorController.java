5e159d3b776d67ca88cb830626734d7f51ac5f47
sfali23 <f.syed.ali@gmail.com>
2017-02-23 07:32:00 -0500

    adding Menubar and Toolbar

--- morphological-analysis-ui-token-editor/src/main/java/com/alphasystem/morphologicalanalysis/ui/tokeneditor/control/controller/TokenEditorController.java
+++ morphological-analysis-ui-token-editor/src/main/java/com/alphasystem/morphologicalanalysis/ui/tokeneditor/control/controller/TokenEditorController.java
@@ -3,13 +3,20 @@ package com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.controller;
 import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.DetailEditorView;
 import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.TokenEditorView;
 import com.alphasystem.morphologicalanalysis.ui.tokeneditor.control.TokenPropertiesView;
+import com.alphasystem.morphologicalanalysis.ui.util.ApplicationHelper;
 import com.alphasystem.morphologicalanalysis.wordbyword.model.Token;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 /**
  * @author sali
@@ -20,6 +27,8 @@ public class TokenEditorController extends BorderPane {
     @Autowired private TokenEditorView control;
     @Autowired private TokenPropertiesView tokenPropertiesView;
     @Autowired private DetailEditorView detailEditorView;
+    @FXML private MenuBar menuBar;
+    @FXML private ToolBar toolBar;
 
     private static String getTokenPropertiesViewPaneTitle(Token token) {
         String title = "Token Properties - %s";
@@ -28,16 +37,29 @@ public class TokenEditorController extends BorderPane {
     }
 
     @PostConstruct
-    void postConstruct() {
-        tokenPropertiesView.setToken(control.getToken());
-        detailEditorView.setLocation(tokenPropertiesView.getSelectedLocation());
+    void postConstruct() throws IOException, URISyntaxException {
+        ApplicationHelper.loadFxml(control, this);
+    }
+
+    @FXML
+    void initialize() {
         TitledPane tokenPropertiesViewPane = new TitledPane(getTokenPropertiesViewPaneTitle(this.control.getToken()), tokenPropertiesView);
         control.tokenProperty().addListener((observable, oldValue, newValue) -> {
             tokenPropertiesViewPane.setText(getTokenPropertiesViewPaneTitle(newValue));
             tokenPropertiesView.setToken(newValue);
         });
+
+        tokenPropertiesView.setToken(control.getToken());
+        detailEditorView.setLocation(tokenPropertiesView.getSelectedLocation());
         detailEditorView.locationProperty().bind(tokenPropertiesView.selectedLocationProperty());
-        setTop(tokenPropertiesViewPane);
+
+        final VBox top = (VBox) getTop();
+        top.getChildren().add(tokenPropertiesViewPane);
         setCenter(detailEditorView);
+    }
+
+    @FXML
+    private void onSave(){
+
     }
 }
