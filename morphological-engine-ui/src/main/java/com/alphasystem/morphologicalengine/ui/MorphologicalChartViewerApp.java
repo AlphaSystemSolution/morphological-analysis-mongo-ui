353b7072f46ecd9468c11b269287f315b1d28ba2
sfali23 <f.syed.ali@gmail.com>
2020-10-30 16:42:28 -0400

    updated

--- morphological-engine-ui/src/main/java/com/alphasystem/morphologicalengine/ui/MorphologicalChartViewerApp.java
+++ morphological-engine-ui/src/main/java/com/alphasystem/morphologicalengine/ui/MorphologicalChartViewerApp.java
@@ -2,12 +2,14 @@ package com.alphasystem.morphologicalengine.ui;
 
 import com.alphasystem.arabic.model.ArabicLetterType;
 import com.alphasystem.arabic.model.NamedTemplate;
+import com.alphasystem.fx.ui.Browser;
 import com.alphasystem.morphologicalanalysis.morphology.model.ConjugationData;
 import com.alphasystem.morphologicalanalysis.morphology.model.ConjugationTemplate;
 import com.alphasystem.morphologicalanalysis.morphology.model.RootLetters;
 import javafx.application.Application;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -47,7 +49,12 @@ public class MorphologicalChartViewerApp extends Application {
         MorphologicalChartViewerControl morphologicalChartViewer = new MorphologicalChartViewerControl();
         morphologicalChartViewer.setMorphologicalChart(morphologicalChart);*/
 
-        Scene scene = new Scene(new Pane());
+        final Browser browser = new Browser();
+        browser.loadUrl("http://ejtaal.net/aa/index.html#bwq=fEl");
+        final BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(browser);
+
+        Scene scene = new Scene(borderPane);
         primaryStage.setMaximized(true);
         primaryStage.setScene(scene);
         primaryStage.show();
